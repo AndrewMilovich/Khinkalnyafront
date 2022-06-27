603a26583be67519883eed4ca1bbb60e282215cd
AndrewMilovich <triplle@ukr.net>
2022-06-27 18:46:56 +0300

    add registration and login

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './App.css';
 import UserRegistration from "./components/User/UserRegistration/UserRegistration";
 import {Route, Routes} from "react-router-dom";
 import UserLogin from "./components/User/UserLogin/UserLogin";
+import Layout from "./components/Layout/Layout";
 
 function App() {
     return (
         <div>
-            hello
             <Routes>
-                <Route path={'/auth/registration'} element={<UserRegistration/>}></Route>
-                <Route path={'/auth/login'} element={<UserLogin/>}></Route>
+                <Route path={"/"} element={<Layout/>}>
+                    <Route path={'/auth/registration'} element={<UserRegistration/>}></Route>
+                    <Route path={'/auth/login'} element={<UserLogin/>}></Route>
+                </Route>
             </Routes>
         </div>
     );
